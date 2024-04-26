261b2c2d23f2cbdea2119e502f2d43cabf0658be
Arpit Kumar Pandey <arpipand@cisco.com>
2024-04-26 09:38:58 +0530

    feat: add test for next and prev

--- internal/playbackmanager/playback_manager_test.go
+++ internal/playbackmanager/playback_manager_test.go
@@ -21,7 +21,7 @@ func TestPlaybackManager(t *testing.T) {
 	go func() {
 		_ = playbackManager.Play()
 	}()
-	testStop := false
+	testStop := true
 	if testStop {
 		time.Sleep(time.Second * 9)
 		println("stopping playback of current track")
@@ -32,5 +32,33 @@ func TestPlaybackManager(t *testing.T) {
 		// println("testing next()")
 		// _ = playbackManager.Next()
 	}
+	<-playbackManager.QueuePlaybackFinished
+}
+func TestNextPrevious(t *testing.T) {
+	musicFiles := []string{
+		"../../music/ricor.flac",
+		"../../music/sample-96kHz24bit.flac",
+		"../../music/sample-3s.mp3",
+		"../../music/sample-9s.mp3",
+		"../../music/sample-12s.mp3",
+	}
+	playbackManager := CreatePlaybackManager()
+	err := playbackManager.AddAudioFilesToQueue(musicFiles...)
+	if err != nil {
+		t.Error(err)
+	}
+	go func() {
+		_ = playbackManager.Play()
+	}()
+	time.Sleep(time.Second * 9)
+	println("Moving to next track")
+	_ = playbackManager.Next()
+	time.Sleep(time.Second * 4)
+	println("Moving to previous track")
+	_ = playbackManager.Previous()
+	time.Sleep(time.Second * 6)
+	println("skipping 2 tracks")
+	_ = playbackManager.Next()
+	_ = playbackManager.Next()
 	<-playbackManager.QueuePlaybackFinished
 }
